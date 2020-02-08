2c07d5a8d9d4848449d65945bbb10fec17e37e8b
lsmierzchalski <lsmierzchalski@gmail.com>
2020-02-08 12:21:17 +0100

    heroku test7

--- server_code.py
+++ server_code.py
@@ -1,189 +1,38 @@
 import os
-import base64
 
 import dash
 import dash_core_components as dcc
-import dash_bootstrap_components as dbc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
-
-import dash_interactive_graphviz
-
-import parser
-
-UPLOAD_DIRECTORY = "app_uploaded_files"
-C4_5_COMMAND = "bin/c45 -f"
-
-if not os.path.exists(UPLOAD_DIRECTORY):
-    os.makedirs(UPLOAD_DIRECTORY)
-
-dot_source = """
-digraph {
-	"94b657ba-4288-11ea-b0bf-54ee75581e6b" [label=outlook]
-	"94b80562-4288-11ea-886a-54ee75581e6b" [label="Play (4.0) "]
-	"94b657ba-4288-11ea-b0bf-54ee75581e6b" -> "94b80562-4288-11ea-886a-54ee75581e6b" [label="= overcast:"]
-	"94b82c74-4288-11ea-a58d-54ee75581e6b" [label=humidity]
-	"94b657ba-4288-11ea-b0bf-54ee75581e6b" -> "94b82c74-4288-11ea-a58d-54ee75581e6b" [label="= sunny:"]
-	"94b82c75-4288-11ea-afd3-54ee75581e6b" [label="Play (2.0) "]
-	"94b82c74-4288-11ea-a58d-54ee75581e6b" -> "94b82c75-4288-11ea-afd3-54ee75581e6b" [label="<= 75"]
-	"94b82c76-4288-11ea-a3a7-54ee75581e6b" [label="Don't Play (3.0) "]
-	"94b82c74-4288-11ea-a58d-54ee75581e6b" -> "94b82c76-4288-11ea-a3a7-54ee75581e6b" [label="> 75"]
-	"94b82c77-4288-11ea-827f-54ee75581e6b" [label=windy]
-	"94b657ba-4288-11ea-b0bf-54ee75581e6b" -> "94b82c77-4288-11ea-827f-54ee75581e6b" [label="= rain:"]
-	"94b82c78-4288-11ea-8758-54ee75581e6b" [label="Don't Play (2.0) "]
-	"94b82c77-4288-11ea-827f-54ee75581e6b" -> "94b82c78-4288-11ea-8758-54ee75581e6b" [label="= true:"]
-	"94b82c79-4288-11ea-bb95-54ee75581e6b" [label="Play (3.0) "]
-	"94b82c77-4288-11ea-827f-54ee75581e6b" -> "94b82c79-4288-11ea-bb95-54ee75581e6b" [label="= false:"]
-}
-"""
-
-dot_source_error = """
-digraph {
-	"94b657ba-4288-11ea-b0bf-54ee75581e6b" [label=ERROR]
-}
-"""
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink("Link", href="#")),
-    ],
-    brand="Projekt C4.5",
-    brand_href="#",
-    sticky="top",
-)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-body = dbc.Container(
-    [
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        dcc.Upload(
-                            id='upload-names',
-                            children=html.Div(
-                                html.Button('Załaduj plik .NAMES'),
-                                style={
-                                    'height': '40px',
-                                }
-                            ),
-                            # Allow multiple files to be uploaded
-                            multiple=False,
-                            accept=".NAMES"
-                        ),
-                        html.Div(
-                            id='output-names-upload',
-                            style={
-                                'height': '40px',
-                            }
-                        ),
-                    ],
-                    md=6
-                ),
-                dbc.Col(
-                    [
-                        dcc.Upload(
-                            id='upload-data',
-                            children=html.Div(
-                                html.Button('Załaduj plik .DATA'),
-                                style={
-                                    'height': '40px',
-                                }),
-                            # Allow multiple files to be uploaded
-                            multiple=False,
-                            accept=".DATA"
-                        ),
-                        html.Div(
-                            id='output-data-upload',
-                            style={
-                                'height': '40px',
-                            }
-                        ),
-                    ],
-                    md=6
-                ),
-                dbc.Col(
-                    [
-                        dbc.Button("Generuj drzewo", id="create_tree_button", color="secondary"),
-                        dash_interactive_graphviz.DashInteractiveGraphviz(
-                            id="tree",
-                            dot_source=dot_source,
-                            style={
-                                'height': '75vh',
-                            }
-                        ),
-                    ],
-                    md=12
-                ),
-            ]
-        )
-    ],
-    className="mt-4",
-)
+server = app.server
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = html.Div([
+    html.H2('Hello World Test'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
 
-app.layout = html.Div([navbar, body])
-
-def save_file(name, content):
-    """Decode and store a file uploaded with Plotly Dash."""
-    data = content.encode("utf8").split(b";base64,")[1]
-    with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
-        fp.write(base64.decodebytes(data))
-
-def parse_contents(contents, filename, extension):
-    save_file("file"+extension, contents)
-    return html.Div([
-        html.H5(filename)
-    ])
-
-@app.callback(Output('output-names-upload', 'children'),
-              [Input('upload-names', 'contents')],
-              [State('upload-names', 'filename'),
-               State('upload-names', 'last_modified')])
-def update_output_names(content, name, date):
-    if content is not None:
-        children = [
-            parse_contents(content, name, '.names')
-        ]
-        return children
-
-@app.callback(Output('output-data-upload', 'children'),
-              [Input('upload-data', 'contents')],
-              [State('upload-data', 'filename'),
-               State('upload-data', 'last_modified')])
-def update_output_date(content, name, date):
-    if content is not None:
-        children = [
-            parse_contents(content, name, '.data')
-        ]
-        return children
-
-@app.callback(Output('tree', 'dot_source'), [Input('create_tree_button', 'n_clicks')])
-def update_tree(number_of_times_button_has_clicked):
-    cmd = C4_5_COMMAND + " " + UPLOAD_DIRECTORY+"/file"  # + " > " + RESULT_FILE_NAME
-    lines = os.popen(cmd).readlines()
-    print(lines)
-
-    try:
-        tree = parser.Tree()
-        select_lines = parser.get_tree_lines(lines)
-        tree.root = parser.division_tree(select_lines, 0)
-        return str(parser.generate_graphviz(tree))
-    except:
-
-        return dot_source_error
-
-    return dot_source
-
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    text1 = os.popen("ls -la").read()
+    text2 = os.popen("ls -la bin").read()
+    text3 = os.popen("chmod +x bin/average").read()
+    text4 = os.popen("chmod +x bin/c45").read()
+    text5 = os.popen("chmod +x bin/c4.5rules").read()
+    text6 = os.popen("chmod +x bin/consult").read()
+    text7 = os.popen("chmod +x bin/consultr").read()
+    text8 = os.popen("ls -la bin").read()
+    text9 = os.popen("bin/c45 -f golf").read()
+    return str(text1)+ "|" + str(text2) + "|" + str(text3) + "|" + str(text4) + "|" + str(text5) + "|" + str(text6) + "|" + str(text7) + "|" + str(text8) + "|" + str(text9)
 
 if __name__ == '__main__':
-    os.popen("chmod +x bin/average")
-    os.popen("chmod +x bin/c45")
-    os.popen("chmod +x bin/c4.5rules")
-    os.popen("chmod +x bin/consult")
-    os.popen("chmod +x bin/consultr")
-    app.run_server(
-        debug=True
-    )
+    app.run_server(debug=True)
